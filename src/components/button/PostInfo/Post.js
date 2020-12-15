60ed53947ef6b492034733bc0a0b90ebef94277d
QWR3 <anton7@i.ua>
2020-12-15 19:58:44 +0200

    4

--- src/components/button/PostInfo/Post.js
+++ src/components/button/PostInfo/Post.js
@@ -5,16 +5,16 @@ class Post extends Component {
         return (
             <div>
                 {/*{console.log(posts[0])}*/}
-                <h2>id-{posts[0].id}<br/>title-{posts[0].title}<br/>body-{posts[0].body}</h2>
-                <h2>id-{posts[1].id}<br/>title-{posts[1].title}<br/>body-{posts[1].body}</h2>
-                <h2>id-{posts[2].id}<br/>title-{posts[2].title}<br/>body-{posts[2].body}</h2>
-                <h2>id-{posts[3].id}<br/>title-{posts[3].title}<br/>body-{posts[3].body}</h2>
-                <h2>id-{posts[4].id}<br/>title-{posts[4].title}<br/>body-{posts[4].body}</h2>
-                <h2>id-{posts[5].id}<br/>title-{posts[5].title}<br/>body-{posts[5].body}</h2>
-                <h2>id-{posts[6].id}<br/>title-{posts[6].title}<br/>body-{posts[6].body}</h2>
-                <h2>id-{posts[7].id}<br/>title-{posts[7].title}<br/>body-{posts[7].body}</h2>
-                <h2>id-{posts[8].id}<br/>title-{posts[8].title}<br/>body-{posts[8].body}</h2>
-                <h2>id-{posts[9].id}<br/>title-{posts[9].title}<br/>body-{posts[9].body}</h2>
+                <h2>{posts[0].id}. {posts[0].title}<br/>{posts[0].body}</h2>
+                <h2>{posts[1].id}. {posts[1].title}<br/>{posts[1].body}</h2>
+                <h2>{posts[2].id}. {posts[2].title}<br/>{posts[2].body}</h2>
+                <h2>{posts[3].id}. {posts[3].title}<br/>{posts[3].body}</h2>
+                <h2>{posts[4].id}. {posts[4].title}<br/>{posts[4].body}</h2>
+                <h2>{posts[5].id}. {posts[5].title}<br/>{posts[5].body}</h2>
+                <h2>{posts[6].id}. {posts[6].title}<br/>{posts[6].body}</h2>
+                <h2>{posts[7].id}. {posts[7].title}<br/>{posts[7].body}</h2>
+                <h2>{posts[8].id}. {posts[8].title}<br/>{posts[8].body}</h2>
+                <h2>{posts[9].id}. {posts[9].title}<br/>{posts[9].body}</h2>
             </div>
         );
     }
